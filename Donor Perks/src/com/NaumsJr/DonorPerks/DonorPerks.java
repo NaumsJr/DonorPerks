45f34cada97764ca41e083157d4f9808c68869f1
46595903 <NaumsJr@gmail.com>
2019-08-01 14:51:52 +1000

    Updates

---
+++ Donor Perks/src/com/NaumsJr/DonorPerks/DonorPerks.java
@@ -0,0 +1,13 @@
+package com.NaumsJr.DonorPerks;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class DonorPerks extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getServer().getConsoleSender().sendMessage("Congrats");
+	}
+}
